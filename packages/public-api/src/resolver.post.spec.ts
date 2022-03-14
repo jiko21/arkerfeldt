5182682c5eeac6805ed197a0097c3b28077cb991
jiko21 <daikikojima0531@icloud.com>
2022-03-14 23:14:12 +0900

    fix: mock prisma client

--- packages/public-api/src/resolver.post.spec.ts
+++ packages/public-api/src/resolver.post.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma/prisma.service';
 import { PostResolver } from './resolver.post';
+jest.mock('@prisma/client')
 
 describe('resolver.post', () => {
   let resolver: PostResolver;
